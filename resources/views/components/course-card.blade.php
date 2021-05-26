eea0cd3d7e0b16648728c25a651d2c144e468ce2
Juanjosexdd <juanjosexdd7@gmail.com>
2021-05-26 13:11:51 -0400

    Nuevas clases Agregadas

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,11 +1,11 @@
 @props(['course'])
 
 
-<article class="bg-white shadow-lg rounded overflow-hidden">
+<article class="card">
     <img class="h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
     
-    <div class="px-6 py-4">
-        <h1 class="text-xl text-gray-700 mb-2 leading-6"> {{Str::limit($course->title, 40)}} </h1>
+    <div class="card-body">
+        <h1 class="card-title"> {{Str::limit($course->title, 40)}} </h1>
         <h1 class="text-gray-500 text-sm mb-2">Prof: {{$course->teacher->name}}</h1>
         <div class="flex">
             <ul class="flex text-sm">
@@ -31,6 +31,6 @@
             </p>
         </div>
         <div class="flex justify-center mt-4">
-            <a href=" {{route('courses.show', $course)}} " type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full mt-2 rounded">Más Información</a>
+            <a href=" {{route('courses.show', $course)}} " type="submit" class="btn-block mt-4 btn btn-primary">Más Información</a>
     </div>
 </article>
