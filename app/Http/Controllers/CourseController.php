fad535412c4026b532825eaef467c927685a656f
JuanJosexD <juanjosexdd7@gmail.com>
2021-05-31 22:04:08 -0400

    Vista Show Terminada, buscador global terminado, habilitado boton para matricularse

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -14,6 +14,21 @@ class CourseController extends Controller
 
     public function show(Course $course)
     {
-        return view('course.show', compact('course'));
+        // Declaramos una variable similares para acceder a todos los
+        // cursos que comparten la misma categoria que el curso en cuestion
+        //
+        $similares = Course::where('category_id', $course->category_id)
+                           ->where('id', '!=', $course->id)   
+                           ->where('status', 3)
+                           ->latest('id')    
+                           ->take(5)         
+                           ->get();
+        return view('course.show', compact('course','similares'));
+    }
+
+    public function enrolled(Course $course)
+    {
+        $course->studens()->attach(auth()->user()->id);
+        return redirect()->route('courses.status', $course);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('cursos', [CourseController::class, 'index'])->name('courses.index');
 
 Route::get('cursos/{course}', [CourseController::class, 'show'])->name('courses.show');
+Route::post('courses/{course}/enrolled',[CourseController::class, 'enrolled'])->middleware('auth')->name('courses.enrolled');
+
+Route::get('course-status/{course}', function($course){return "hola";})->name('courses.status');
